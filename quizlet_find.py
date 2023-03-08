e8ded75231694b33824befaf61a6945f30516ed4
MattTiti <mtiti3@vt.edu>
2023-03-07 23:15:23 -0500

    description functions

--- quizlet_find.py
+++ quizlet_find.py
@@ -16,18 +16,19 @@ def search():
         query = request.form["query"]
         service = build("customsearch", "v1", developerKey='AIzaSyB0Y2mhvYWOmFCFj35rA_8FJKGYEGonkQ0')
         result = service.cse().list(q=query, cx='756fae1a68b4e48dd').execute()
-
+        
         links = []
         titles = []
         descriptions = []
         for item in result['items']:
             link = item['link']
+            snip = item['snippet']
+            descriptions.append(snip)
             if link.startswith("https://quizlet.com/"):
                 links.append(link)
                 titles.append(item['title'])
                 descriptions.append(item['snippet'])
-
-        return render_template("index.html", links=links, titles=titles)
+        return render_template("index.html", links=links, titles=titles, descriptions=descriptions)
     return render_template("index.html")
 
 if __name__ == "__main__":
